ef5f7fa9d05cd7411589f387bd95da6c03f84ee3
Adrianus West <adrianus_west@protonmail.com>
2022-10-16 17:51:02 +0300

    Update: project refactoring.

--- tests/gendiff/test_gendiff.py
+++ tests/gendiff/test_gendiff.py
@@ -2,60 +2,6 @@ from gendiff.gendiff import generate_diff
 
 from tests.fixtures.expected_strings import NESTED_PLAIN_DIFF_STR_12
 from tests.fixtures.expected_strings import NESTED_STYLISH_DIFF_STR_12
-from tests.fixtures.expected_strings import PLAIN_DIFF_STR_12
-from tests.fixtures.expected_strings import PLAIN_DIFF_STR_21
-
-
-def test_gendiff_stylish_plain():
-    # Test jsons
-    assert generate_diff(
-        './tests/fixtures/plain1.json',
-        './tests/fixtures/plain2.json',
-        'stylish',
-    ) == PLAIN_DIFF_STR_12
-
-    assert generate_diff(
-        './tests/fixtures/plain2.json',
-        './tests/fixtures/plain1.json',
-        'stylish',
-    ) == PLAIN_DIFF_STR_21
-
-    # Test yamls and # Test yamls with jsons in in various combinations
-    assert generate_diff(
-        './tests/fixtures/plain1.yaml',
-        './tests/fixtures/plain2.yaml',
-        'stylish',
-    ) == PLAIN_DIFF_STR_12
-
-    assert generate_diff(
-        './tests/fixtures/plain1.json',
-        './tests/fixtures/plain2.yaml',
-        'stylish',
-    ) == PLAIN_DIFF_STR_12
-
-    assert generate_diff(
-        './tests/fixtures/plain1.yml',
-        './tests/fixtures/plain2.json',
-        'stylish',
-    ) == PLAIN_DIFF_STR_12
-
-    assert generate_diff(
-        './tests/fixtures/plain2.yml',
-        './tests/fixtures/plain1.yml',
-        'stylish',
-    ) == PLAIN_DIFF_STR_21
-
-    assert generate_diff(
-        './tests/fixtures/plain2.yml',
-        './tests/fixtures/plain1.json',
-        'stylish',
-    ) == PLAIN_DIFF_STR_21
-
-    assert generate_diff(
-        './tests/fixtures/plain2.json',
-        './tests/fixtures/plain1.yaml',
-        'stylish',
-    ) == PLAIN_DIFF_STR_21
 
 
 def test_gendiff_stylish_nested():
